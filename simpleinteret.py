5dd948af7df3595db529f011e8317f50c8d62464
Rushikesh Badgujar <82447941+rishi7822@users.noreply.github.com>
2022-11-27 23:37:27 +0530

    Add files via upload

---
+++ simpleinteret.py
@@ -0,0 +1,24 @@
+#python program to find simple interest
+def simple_interest(p,t,r):
+    print("the principal is",p)
+    print("the time period is",t)
+    print("the rate is",r)
+
+    si = (p*t*r)/100
+
+    print("the simple interest is",si)
+    return si
+
+simple_interest(35,40,50)
+###################################
+def simple_interest(p,t,r):
+    print("the principal is:",p)
+    print("the time period is:",t)
+    print("the rate is:",r)
+
+    si = (p*t*r)/100
+
+    print("the simple interest is",si)
+    return si
+
+simple_interest(40,58,68)
